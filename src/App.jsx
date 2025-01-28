e2620a02d9d29fd97831a59c5dc17fce629f3735
parby8605 <lhj971011@gmail.com>
2025-01-28 15:04:52 +0900

    Portal tutorial

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,56 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { createContext, useContext, useState } from 'react'
 import './App.css'
+import { createPortal } from 'react-dom'
 
-function App() {
-  const [count, setCount] = useState(0)
+const ModalContext = createContext({ show: (content) => {}, close: () => {} })
+
+function ModalContextProvier({ children }) {
+  const [modal, setModal] = useState({ open: false, content: <></> })
+
+  function show(content) {
+    setModal({ open: true, content })
+  }
+
+  function close() {
+    setModal({ open: false, content: <></> })
+  }
 
   return (
+    <ModalContext.Provider value={{ show, close }}>
+      {children}
+      {modal.open &&
+        createPortal(
+          <dialog open>
+            {modal.content}
+            <button onClick={() => close()}>닫기</button>
+          </dialog>,
+          document.body,
+        )}
+    </ModalContext.Provider>
+  )
+}
+
+function Modal1Button() {
+  const { show } = useContext(ModalContext)
+  return <button onClick={() => show(<h3>Modal 1</h3>)}>Open Modal1</button>
+}
+function Modal2Button() {
+  const { show } = useContext(ModalContext)
+  return <button onClick={() => show(<h3>Modal 2</h3>)}>Open Modal2</button>
+}
+function Modal3Button() {
+  const { show } = useContext(ModalContext)
+  return <button onClick={() => show(<h3>Modal 3</h3>)}>Open Modal3</button>
+}
+
+function App() {
+  return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <ModalContextProvier>
+        <Modal1Button />
+        <Modal2Button />
+        <Modal3Button />
+      </ModalContextProvier>
     </>
   )
 }
