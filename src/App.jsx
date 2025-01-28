de3737691529e4a2b6eb9280e756ad10b52772a8
parby8605 <lhj971011@gmail.com>
2025-01-28 16:49:48 +0900

    useState의 2가지 방식 사용하여 레벨업, 전직 구현하기

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,35 @@
-import { useState } from 'react'
+import { useEffect, useMemo, useState } from 'react'
 
 import './App.css'
 
 function App() {
-  return <></>
+  const [level, setLevel] = useState(1)
+  const [title, setTitle] = useState('Novice')
+
+  useEffect(() => {
+    if (level >= 15 && level < 30) {
+      setTitle('1차전직')
+    } else if (level >= 30) {
+      setTitle('2차전직')
+    }
+  }, [level])
+
+  const levelUp_1 = () => {
+    const plusLevel = level + 1
+    setLevel(plusLevel)
+  }
+
+  const levelUp_2 = () => {
+    setLevel((prev) => prev + 1)
+  }
+
+  return (
+    <>
+      <div style={{ marginBottom: 10 }}>{level}</div>
+      <div style={{ marginBottom: 10 }}>{title}</div>
+      <button onClick={levelUp_2}>레벨업</button>
+    </>
+  )
 }
 
 export default App
