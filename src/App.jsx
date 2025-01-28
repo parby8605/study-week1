3fa129cc65677a291c9b2ac1aeebcb9a70727b8d
parby8605 <lhj971011@gmail.com>
2025-01-28 16:17:26 +0900

    useCallback과 memo, useMemo 실습

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,35 @@
-import { useState } from 'react'
+import { memo, useCallback, useMemo, useState } from 'react'
+import '@/App.css'
 
-import './App.css'
+const Validation = memo(function Validation({ valid }) {
+  console.log('- rerendered : Validation')
+  return <div>{valid ? <p>성년입니다</p> : <p style={{ color: 'red' }}>미성년입니다</p>}</div>
+})
+
+const Button = memo(function Button({ onClick }) {
+  console.log('- rerendered : Button')
+  return <button onClick={onClick}>증가</button>
+})
 
 function App() {
-  return <></>
+  const [age, setAge] = useState(0)
+
+  const valid = useMemo(() => {
+    console.log('calculated')
+    return age >= 19 ? true : false
+  }, [age >= 19])
+
+  const increase = useCallback(() => {
+    setAge((previous) => previous + 1)
+  }, [])
+
+  return (
+    <>
+      <p>{age}</p>
+      <Validation valid={valid} />
+      <Button onClick={increase} />
+    </>
+  )
 }
 
 export default App
