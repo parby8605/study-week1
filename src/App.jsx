2a6f1a6b6212d1293f623624a730859ed9dd30ca
parby8605 <lhj971011@gmail.com>
2025-01-28 17:53:14 +0900

    19세 기준 미성년자 / 성년 에 따른 놀이공원 입장료 변경 로직

--- src/App.jsx
+++ src/App.jsx
@@ -3,33 +3,21 @@ import { useState } from 'react'
 import './App.css'
 
 function App() {
+  const FEE_ADULT = 20000
+  const FEE_NON_ADULT = 10000
   const [age, setAge] = useState(0)
-  const [valid, setValid] = useState(false /* 성년 여부 */)
 
   //setState 직접 값 설정
   const handleAge = (e) => {
-    const inputAge = e.currentTarget.value
+    const inputAge = Number(e.currentTarget.value)
     setAge(inputAge)
-    setValid(inputAge >= 19)
-  }
-
-  //setState 함수형 업데이트트
-  const increaseAge = () => {
-    setAge((prev) => prev + 1)
-    setValid((prev) => Number(age) + 1 >= 19)
-  }
-
-  const decreaseAge = () => {
-    setAge((prev) => (prev > 0 ? prev - 1 : 0))
-    setValid((prev) => Number(age) - 1 >= 19)
   }
 
   return (
     <>
       <input type='number' value={age} onChange={handleAge} />
-      <button onClick={increaseAge}>Increase</button>
-      <button onClick={decreaseAge}>Decrease</button>
-      {valid ? <div>성년입니다.</div> : <div style={{ color: 'red' }}>미성년입니다.</div>}
+      {age >= 19 ? <div>성년입니다.</div> : <div style={{ color: 'red' }}>미성년입니다.</div>}
+      <div>{age >= 19 ? FEE_ADULT : FEE_NON_ADULT}원</div>
     </>
   )
 }
