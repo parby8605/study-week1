f9b5b1829ea505440659f99f1222a051c7b23b40
parby8605 <lhj971011@gmail.com>
2025-01-28 16:05:24 +0900

    react memo tutorial

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,20 @@
-import { useState } from 'react'
+import { useMemo, useState } from 'react'
 
 import './App.css'
 
 function App() {
-  return <></>
-}
+  const [age, setAge] = useState(0)
 
+  const valid = useMemo(() => {
+    console.log('calculated')
+    return age >= 19 ? true : false
+  }, [age >= 19])
+  return (
+    <div>
+      <p>{age}</p>
+      <div>{valid ? <p>Adult</p> : <p>Underage</p>}</div>
+      <button onClick={() => setAge(age + 1)}>증가</button>
+    </div>
+  )
+}
 export default App
