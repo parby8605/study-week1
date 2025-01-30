329af804c022dc5f04002df56b672de58fed2472
parby8605 <lhj971011@gmail.com>
2025-01-30 17:02:25 +0900

    useReducer 실습

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,24 @@
-import { useState } from 'react'
+import { useReducer, useState } from 'react'
 
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const initialState = 0
 
+  const reducer = (state, action) => {
+    switch (action.type) {
+      case 'INCREMENT':
+        return state + action.scale
+      case 'DECREMENT':
+        return state - action.scale
+    }
+  }
+  const [count, dispatch] = useReducer(reducer, initialState)
   return (
     <>
       <div>{count}</div>
-      <button onClick={() => setCount((prev) => prev + 10)}>증가</button>
-      <button onClick={() => setCount((prev) => prev - 10)}>감소</button>
+      <button onClick={() => dispatch({ type: 'INCREMENT', scale: 10 })}>증가</button>
+      <button onClick={() => dispatch({ type: 'DECREMENT', scale: 10 })}>감소</button>
     </>
   )
 }
