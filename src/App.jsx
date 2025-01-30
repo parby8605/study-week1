39c0fe4576ef9e1f62bf78e76872907066099bfe
parby8605 <lhj971011@gmail.com>
2025-01-30 16:53:49 +0900

    init problem

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,15 @@ import { useState } from 'react'
 import './App.css'
 
 function App() {
-  return <></>
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div>{count}</div>
+      <button onClick={() => setCount((prev) => prev + 10)}>증가</button>
+      <button onClick={() => setCount((prev) => prev - 10)}>감소</button>
+    </>
+  )
 }
 
 export default App
