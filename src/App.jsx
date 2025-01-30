0250579b5efa948841508e3afc284f9c2939e7a1
parby8605 <lhj971011@gmail.com>
2025-01-30 13:09:42 +0900

    ref활용해서 불필요한 렌더링 방지

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,20 @@
-import { useState } from 'react'
+import { useRef, useState } from 'react'
 
 import './App.css'
 
 function App() {
-  const [age, setAge] = useState(0)
   const [valid, setValid] = useState(false /* 성년 여부 */)
-
-  //setState 직접 값 설정
-  const handleAge = (e) => {
-    const inputAge = e.currentTarget.value
-    setAge(inputAge)
-    setValid(inputAge >= 19)
-  }
-
-  //setState 함수형 업데이트트
-  const increaseAge = () => {
-    setAge((prev) => prev + 1)
-    setValid((prev) => Number(age) + 1 >= 19)
-  }
-
-  const decreaseAge = () => {
-    setAge((prev) => (prev > 0 ? prev - 1 : 0))
-    setValid((prev) => Number(age) - 1 >= 19)
-  }
+  const ageRef = useRef()
 
   return (
     <>
-      <input type='number' value={age} onChange={handleAge} />
-      <button onClick={increaseAge}>Increase</button>
-      <button onClick={decreaseAge}>Decrease</button>
+      <input
+        type='number'
+        ref={ageRef}
+        onChange={(e) => {
+          setValid(Number(e.target.value) >= 19)
+        }}
+      />
       {valid ? <div>성년입니다.</div> : <div style={{ color: 'red' }}>미성년입니다.</div>}
     </>
   )
