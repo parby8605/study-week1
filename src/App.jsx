9176826f7cc53756cda97cc2b04ca184cb8fe54a
parby8605 <lhj971011@gmail.com>
2025-01-30 15:47:07 +0900

    옵저버 활용하여 헤더 구현

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,52 @@
-import { useState } from 'react'
+import { useEffect, useRef, useState } from 'react'
 
 import './App.css'
 
+function Header({ title, showTitle }) {
+  return (
+    <div style={{ position: 'sticky', top: 0, height: 60, backgroundColor: 'white' }}>
+      {showTitle && title}
+    </div>
+  )
+}
+
+function Post({ title, handleHeader }) {
+  const titleRef = useRef(null)
+  const content =
+    'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.\n\nLorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.\n\nLorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.\n\nLorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim'
+
+  useEffect(() => {
+    const observer = new IntersectionObserver(([entry]) => {
+      if (entry.intersectionRatio) {
+        handleHeader(true)
+      } else if (!entry.intersectionRatio) {
+        handleHeader(false)
+      }
+    })
+
+    observer.observe(titleRef.current)
+
+    return () => {
+      observer.disconnect()
+    }
+  }, [])
+  return (
+    <div>
+      <h3 ref={titleRef}>{title}</h3>
+      <div>{content}</div>
+    </div>
+  )
+}
+
 function App() {
-  return <></>
+  const [isTitleVisible, setIsTitleVisible] = useState(true)
+  const title = 'Lorem ipsum'
+  return (
+    <>
+      <Header title={title} showTitle={!isTitleVisible} />
+      <Post title={title} handleHeader={setIsTitleVisible} />
+    </>
+  )
 }
 
 export default App
