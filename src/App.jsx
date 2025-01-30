e3a7fbeaf66c9960a4e1bdfe813817b7daceca30
parby8605 <lhj971011@gmail.com>
2025-01-30 14:51:36 +0900

    리렌더 없이 비디오 링크 전환 및 텍스트 컬러 전환

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,49 @@
-import { useState } from 'react'
+import { useRef, useState } from 'react'
 
 import './App.css'
 
 function App() {
-  return <></>
+  const sources = [
+    'https://vjs.zencdn.net/v/oceans.mp4',
+    'https://lamberta.github.io/html5-animation/examples/ch04/assets/movieclip.mp4',
+  ]
+  const colorRef = useRef(null)
+  const videoRef = useRef(null)
+  console.log('- rerendered')
+
+  const handleClick = () => {
+    if (colorRef.current.style.color === 'red') {
+      colorRef.current.style.color = 'black'
+    } else {
+      colorRef.current.style.color = 'red'
+    }
+  }
+
+  return (
+    <>
+      <div>
+        <div ref={colorRef}>apple</div>
+        <button onClick={handleClick}>변경</button>
+      </div>
+      <div>
+        <video ref={videoRef} autoPlay controls width={500} muted />
+      </div>
+      <button
+        onClick={() => {
+          videoRef.current.src = sources[0]
+        }}
+      >
+        전환 1
+      </button>
+      <button
+        onClick={() => {
+          videoRef.current.src = sources[1]
+        }}
+      >
+        전환 2
+      </button>
+    </>
+  )
 }
 
 export default App
