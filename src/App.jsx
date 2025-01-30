0e17e0bbc19d1ad0f5e2acb6db67b14cd33a8560
parby8605 <lhj971011@gmail.com>
2025-01-30 09:44:17 +0900

    엔터시 input태그 되돌리기 및 확인/취소버튼 구현

--- src/App.jsx
+++ src/App.jsx
@@ -2,8 +2,62 @@ import { useState } from 'react'
 
 import './App.css'
 
-function ListItem({ children }) {
-  return <li>{children}</li>
+function ListItem({ name, age, desc, handleDesc }) {
+  const [isActive, setIsActive] = useState(false)
+  const [newDesc, setNewDesc] = useState(desc)
+
+  const handleKeyDown = (e) => {
+    if (e.key === 'Enter') {
+      handleDesc(newDesc)
+      setIsActive(false)
+    }
+  }
+  return (
+    <>
+      <li
+        onClick={() => {
+          setIsActive((prev) => !prev)
+        }}
+      >
+        {name} | {age} |{' '}
+        {isActive ? (
+          <>
+            <input
+              value={newDesc}
+              onChange={(e) => {
+                setNewDesc(e.target.value)
+              }}
+              onClick={(e) => e.stopPropagation()}
+              onMouseDown={(e) => e.stopPropagation()}
+              onKeyDown={handleKeyDown}
+            ></input>
+            <button
+              onClick={(e) => {
+                e.stopPropagation()
+                handleDesc(newDesc)
+
+                setIsActive((previous) => !previous)
+              }}
+            >
+              확인
+            </button>
+            <button
+              onClick={(e) => {
+                e.stopPropagation()
+                handleDesc(desc)
+                setNewDesc(desc)
+                setIsActive((previous) => !previous)
+              }}
+            >
+              취소
+            </button>
+          </>
+        ) : (
+          desc
+        )}
+      </li>
+    </>
+  )
 }
 
 function UnorderedList({ children }) {
@@ -15,26 +69,36 @@ function OrderedList({ children }) {
 }
 
 function App() {
-  const items = [
+  const [items, setItems] = useState([
     { name: 'Aaron', age: 10, desc: '안녕하세요' },
     { name: 'Baron', age: 30, desc: '반갑습니다' },
     { name: 'Caron', age: 22, desc: '처음뵙겠습니다' },
     { name: 'Daron', age: 17, desc: '보고싶었습니다' },
-  ]
+  ])
+
+  const handleDesc = (index, newDesc) => {
+    const updateDesc = [...items]
+    updateDesc[index].desc = newDesc
+    setItems(updateDesc)
+  }
   return (
     <>
       <div>
         <OrderedList>
-          <ListItem>Unordered List Item 1</ListItem>
-          <ListItem>Unordered List Item 2</ListItem>
-          <ListItem>Unordered List Item 3</ListItem>
+          <li>Unordered List Item 1</li>
+          <li>Unordered List Item 2</li>
+          <li>Unordered List Item 3</li>
         </OrderedList>
         {/* ol = Ordered List */}
         <UnorderedList>
           {items.map((item, index) => (
-            <ListItem key={index}>
-              {item.name} | {item.age} | {item.desc}
-            </ListItem>
+            <ListItem
+              key={index}
+              name={item.name}
+              age={item.age}
+              desc={item.desc}
+              handleDesc={(desc) => handleDesc(index, desc)}
+            ></ListItem>
           ))}
         </UnorderedList>
       </div>
