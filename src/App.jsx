30d039226bfac59cd2226a18c34d7f0855cac0ef
parby8605 <lhj971011@gmail.com>
2025-01-30 13:51:10 +0900

    ref활용하여 validation을 갖춘 + 불필요한 리렌더 없는 제출페이지 만들기

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,55 @@
-import { useState } from 'react'
+import { forwardRef, useRef, useState } from 'react'
 
 import './App.css'
 
+//validation 포함된 폼 컴포넌트
+// eslint-disable-next-line react/display-name
+const ValidForm = forwardRef(({ title, isRequired = false, length = null }, ref) => {
+  const [requiredValidation, setRequiredValidation] = useState(false)
+  const [lengthValidation, setLengthValidation] = useState(false)
+
+  return (
+    <div>
+      {title} :{' '}
+      <input
+        ref={ref} // 올바르게 ref 전달
+        onChange={(e) => {
+          if (isRequired) setRequiredValidation(e.currentTarget.value.length === 0)
+          if (length) setLengthValidation(e.currentTarget.value.length > length)
+        }}
+      />
+      {requiredValidation && <div style={{ color: 'red' }}>{title}은 필수 입력값입니다.</div>}
+      {lengthValidation && (
+        <div style={{ color: 'red' }}>
+          {title}은 {length}글자 이하여야합니다.
+        </div>
+      )}
+    </div>
+  )
+})
+
 function App() {
-  return <></>
+  const userInfo = {
+    name: useRef(),
+    desc: useRef(),
+    mail: useRef(),
+  }
+
+  const submitInfo = () => {
+    console.log({
+      name: userInfo?.name?.current?.value,
+      desc: userInfo?.desc?.current?.value,
+      mail: userInfo?.mail?.current?.value,
+    })
+  }
+  return (
+    <>
+      <ValidForm title='이름' ref={userInfo.name} isRequired></ValidForm>
+      <ValidForm title='설명' ref={userInfo.desc} isRequired length={10}></ValidForm>
+      <ValidForm title='메일' ref={userInfo.mail} isRequired></ValidForm>
+      <button onClick={submitInfo}>Submit</button>
+    </>
+  )
 }
 
 export default App
