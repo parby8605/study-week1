210a1100098c7274065d587804dad6c93ba49a4d
parby8605 <lhj971011@gmail.com>
2025-01-31 17:09:50 +0900

    useMemo 활용하여 리렌더링 최적화

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,23 @@
-import { useState } from 'react'
+import { useMemo, useState } from 'react'
 
 import './App.css'
 
 function App() {
-  //week1 day 완
-  return <></>
+  const [age, setAge] = useState(0)
+
+  const validate = () => {
+    console.log('calculated')
+    return age >= 19 ? true : false
+  }
+  const valid = useMemo(() => validate(), [age >= 19])
+
+  return (
+    <>
+      <p>{age}</p>
+      <div>{valid ? <p>성년입니다</p> : <p style={{ color: 'red' }}>미성년입니다</p>}</div>
+      <button onClick={() => setAge(age + 1)}>증가</button>
+    </>
+  )
 }
 
 export default App
