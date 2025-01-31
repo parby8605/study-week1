72e83355bcd0050c42beb12ba8b00ab5d7a87b8f
parby8605 <lhj971011@gmail.com>
2025-01-31 15:21:12 +0900

    portal활용한 모달

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,57 @@
-import { useState } from 'react'
+import { createContext, useContext, useEffect, useRef, useState } from 'react'
+import { createPortal } from 'react-dom'
 
-import './App.css'
+const ModalContext = createContext({ show: (content) => {}, close: () => {} })
+
+function ModalContextProvider({ children }) {
+  const [modal, setModal] = useState({ open: false, content: <></> })
+
+  function show(content) {
+    setModal({ open: true, content })
+  }
+
+  function close() {
+    setModal({ open: false, content: <></> })
+  }
+
+  return (
+    <ModalContext.Provider value={{ show, close }}>
+      {children}
+      {modal.open &&
+        createPortal(
+          <dialog open>
+            {modal.content}
+            <button onClick={() => close()}>닫기</button>
+          </dialog>,
+          document.body,
+        )}
+    </ModalContext.Provider>
+  )
+}
+
+function Modal1Button() {
+  const { show } = useContext(ModalContext)
+  return <button onClick={() => show(<h3>Modal 1</h3>)}>1 열기</button>
+}
+
+function Modal2Button() {
+  const { show } = useContext(ModalContext)
+  return <button onClick={() => show(<h3>Modal 2</h3>)}>2 열기</button>
+}
+
+function Modal3Button() {
+  const { show } = useContext(ModalContext)
+  return <button onClick={() => show(<h3>Modal 3</h3>)}>3 열기</button>
+}
 
 function App() {
-  //week1 day 완
-  return <></>
+  return (
+    <ModalContextProvider>
+      <Modal1Button />
+      <Modal2Button />
+      <Modal3Button />
+    </ModalContextProvider>
+  )
 }
 
 export default App
