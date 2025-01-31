7e194252f362adf5ee1e004f8bed94b30fe97295
parby8605 <lhj971011@gmail.com>
2025-01-31 17:13:42 +0900

    useMemo 활용하여 컴포넌트 메모이제이션

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,16 @@
-import { useMemo, useState } from 'react'
+import { memo, useMemo, useState } from 'react'
 
 import './App.css'
+
+const Validation = memo(({ valid }) => {
+  console.log('- rerendered : Validation')
+  return <div>{valid ? <p>성년입니다</p> : <p style={{ color: 'red' }}>미성년입니다</p>}</div>
+})
+
+const Button = memo(({ onClick }) => {
+  console.log('- rerendered : Button')
+  return <button onClick={onClick}>증가</button>
+})
 
 function App() {
   const [age, setAge] = useState(0)
@@ -14,8 +24,8 @@ function App() {
   return (
     <>
       <p>{age}</p>
-      <div>{valid ? <p>성년입니다</p> : <p style={{ color: 'red' }}>미성년입니다</p>}</div>
-      <button onClick={() => setAge(age + 1)}>증가</button>
+      <Validation valid={valid}></Validation>
+      <Button onClick={() => setAge(age + 1)}>증가</Button>
     </>
   )
 }
